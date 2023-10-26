c1c98558cd8e219fede815f1aeaa5de19836a998
sabrinalxt <sabrina.liewxintong@gmail.com>
2023-10-26 18:17:00 +0800

    fixed game

--- public/hangman.js
+++ public/hangman.js
@@ -5,9 +5,6 @@ const wordDisplay = document.getElementById('word-display');
 const guessesRemainingText = document.getElementById('guesses-remaining');
 const keyboard = document.getElementById('keyboard');
 
-// HINT
-const gameResult = document.getElementById('hangman-result');
-
 function startGame(word = words[Math.floor(Math.random() * words.length)], maxGuesses = 6) {
     console.log(word);
     let guessedLetters = new Set();
